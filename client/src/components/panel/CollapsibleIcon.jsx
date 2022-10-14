47d461cd9dd5524253db0da02ef83543541c10b5
almickle <105072712+almickle@users.noreply.github.com>
2022-10-13 18:03:40 -0600

    hamburger icon

--- client/src/components/panel/CollapsibleIcon.jsx
+++ client/src/components/panel/CollapsibleIcon.jsx
@@ -1,3 +1,4 @@
+import HamburgerIcon from '../assets/HamburgerIcon.png'
 
 export default function CollapsibleIcon ( { handleClick, label} ) {
 
@@ -7,7 +8,7 @@ export default function CollapsibleIcon ( { handleClick, label} ) {
     
     return (
         <div id={label} onClick={handleClick} style={{ height: iconHeight, width: iconWidth, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginLeft: 4 }}>
-           <img id={label} src="https://cdn4.iconfinder.com/data/icons/interface-essential-1/24/button-parallel-vertical-lines-menu-navigation-three-hamburger-512.png" alt="drop down menu icon" style={{ height: iconHeight, transform: 'rotate(0deg)', cursor: 'pointer' }} />
+           <img id={label} src={HamburgerIcon} alt="drop down menu icon" style={{ height: iconHeight, transform: 'rotate(0deg)', cursor: 'pointer' }} />
         </div>
     )
 }
