66b73101d96fac9dea76ec7817093ee01f6da9bc
almickle <michaelcdear@gmail.com>
2022-11-21 13:08:00 -0500

    gene library validation fix

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -3,6 +3,6 @@ class Gene < ApplicationRecord
     has_many :annotations
 
     validates :library_id, presence: true
-    validates :symbol, presence: true, uniqueness: true
+    validates :symbol, presence: true
     validates :taxon, presence: true
 end
